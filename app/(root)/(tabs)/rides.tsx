958929451b12f95430c6dc113cc4085380dce463
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-06 18:50:37 +0530

    Sixth Commit

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const rides = () => {
+  return (
+    <div>rides</div>
+  )
+}
+
+export default rides
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,9 +21,11 @@ const RootLayout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
   );
 }
 
