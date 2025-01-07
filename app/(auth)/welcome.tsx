e90d852a29431dc048fe5033283d508383ca31c9
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-07 16:26:41 +0530

    Eighth Commit

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -2,27 +2,35 @@ import { router } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
-import { useRef } from "react";
+import {useRef, useState} from "react";
+import {onboarding} from "@/constants";
 
 const onBoarding = () => {
-
   const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <SafeAreaView>
       <TouchableOpacity
         onPress={() => {
-          router.replace("(auth)/sign-up");
+          router.replace("/(auth)/sign-up");
         }}
         className="w-full flex justify-end items-end p-5"
       >
         <Text className="text-black text-md font-JakartaBold">Skip!</Text>
       </TouchableOpacity>
-      <Swiper ref={swiperRef}
+      <Swiper
+        ref={swiperRef}
         loop={false}
-        dot = {<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]"/>}
+        dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />}
+        activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />}
+        onIndexChanged={(index) => setActiveIndex(index)}
       >
-
+          {onboarding.map((item) => (
+              <View>
+                  <Text>{item.title}</Text>
+              </View>
+          ))}
       </Swiper>
     </SafeAreaView>
   );
---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+export const onboarding = [{
+    id: 1,
+    title: "The Perfect Ride is just a tap away!",
+    description: "You're jounrney begins with Ryde!",
+    image: null,
+    },
+    {
+        id: 2,
+        title: "The Perfect Ride is just a tap away!",
+        description: "You're jounrney begins with Ryde!",
+        image: null,
+    },
+    {
+        id: 3,
+        title: "The Perfect Ride is just a tap away!",
+        description: "You're jounrney begins with Ryde!",
+        image: null,
+    },
+];
