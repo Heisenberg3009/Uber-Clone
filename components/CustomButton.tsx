ba0681a19fb227e8834e0018da895645f62c7196
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-08 20:41:55 +0530

    Thirteenth Commit

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,3 +1,4 @@
+import { ButtonProps } from "@/types/type";
 import { TouchableOpacity } from "react-native";
 
 const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
@@ -15,6 +16,21 @@ const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
   }
 };
 
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+    switch (variant) {
+      case "primary":
+        return "text-black";
+        case "secondary":
+        return "text-grey-100";
+      case "danger":
+        return "text-red-100";
+      case "success":
+        return "text-green-100";
+      default:
+        return "text-white";
+    }
+  };
+
 const CustomButton = ({
   onPress,
   title,
@@ -24,7 +40,7 @@ const CustomButton = ({
   IconRight,
   className,
   ...props
-}) => (
+}: ButtonProps) => (
   <TouchableOpacity
     onPress={onPress}
     className={`w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/700 ${getBgVariantStyle(
@@ -32,7 +48,9 @@ const CustomButton = ({
     )} ${className}`}
   >
     {IconLeft && <IconRight />}
-    <Text>{title}</Text>
+    <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+      {title}
+    </Text>
     {IconRight && <IconRight />}
   </TouchableOpacity>
 );
